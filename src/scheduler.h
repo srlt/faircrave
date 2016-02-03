db882af2950041bd34b1c0b3a528ac660ad8bcee
Sébastien Rouault <srlt@users.noreply.github.com>
2016-02-03 16:09:41 +0100

    La déclaration ne match pas la définition pour router_end

--- src/scheduler.h
+++ src/scheduler.h
@@ -106,7 +106,7 @@ void router_clean(struct router*);
 
 bool router_setonline(struct router*, bool);
 bool router_setnetdev(struct router*, struct netdev*);
-void router_end(struct router*);
+bool router_end(struct router*);
 
 void router_allowip(struct router*, nint, nint);
 
